c74799778700988e252443c862471c4bb841a1e1
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-22 02:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(line,name) {
 function nowServing(line) {
   if (line.length > 0) {
     return `Currently serving ${line[0]}.`
-    line.shift()
+    delete line[0]
 
 }
   else {
