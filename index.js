cbbc248bc967a9f70f977fbd006899dfe42937b4
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-22 02:24:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 function takeANumber(line,name) {
-  var message=""
-  for (let i=0; i<line.length; i++) {
-    if (line[i]===name) {
-      message=`Welcome, ${name}. You are number ${i+1} in line.`
-    }
-  }
-  return message
+  line.push(name)
+  return `Welcome ${name}. You are number ${line.length+1} in line.`
 }
