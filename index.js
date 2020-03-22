dff8fe90d94d9ab8027c8e4cfb9c5695a35cc03a
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-22 02:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ function takeANumber(line,name) {
 }
 
 function nowServing(line) {
+  
   if (line.length > 0) {
     var customer=line.splice(0,1)
     return `Currently serving ${customer}.`
-
-
 }
   else {
     return "There is nobody waiting to be served!"
-
   }
-
 }
